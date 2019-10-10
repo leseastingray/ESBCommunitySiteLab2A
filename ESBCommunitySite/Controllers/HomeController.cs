9dc0511960afd4ce342681a9bad99e5d6deaaa6f
leseastingray <lesea@my.lanecc.edu>
2019-10-10 14:24:16 -0700

    beta skeleton

---
+++ ESBCommunitySite/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ESBCommunitySite.Models;
+
+namespace ESBCommunitySite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Index method
+        public ViewResult Index()
+        {
+            return View();
+        }
+        // History method
+        public ViewResult History()
+        {
+            return View();
+        }
+        // Contact method
+        public ViewResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ESBCommunitySite/Controllers/InfoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESBCommunitySite.Controllers
+{
+    public class InfoController : Controller
+    {
+        // Info method
+        public ViewResult Info()
+        {
+            return View();
+        }
+        // Location method
+        public ViewResult Location()
+        {
+            return View();
+        }
+        // Roster method
+        public ViewResult Roster()
+        {
+            return View();
+        }
+    }
+}
